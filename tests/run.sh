bebd2e0d8dca0509beb089d815e18af259017a93
Martin Dautry <mdautry@steamulo.com>
2016-10-12 15:29:03 +0200

    Initial commit

---
+++ tests/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+vagrant status | grep "running" -q
+if [ $? -ne 0 ]; then
+    vagrant up
+fi
+
+pwd | grep ansible-role-ssh/tests$ -q
+if [ $? -ne 0 ]; then
+    echo "tests must be run in the ansible-role-oco/tests folder"
+    exit 1
+fi
+
+ansible-playbook -i inventory test.yml -u vagrant -e "ansible_ssh_pass=vagrant" \
+--ssh-common-args="-o PubkeyAuthentication=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $@
